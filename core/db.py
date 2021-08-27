3ab73768f34b890758d53766128b5bb916e710f5
wizm ndld <56812006+Euxcbsks@users.noreply.github.com>
2021-08-27 06:43:30 +0000

    Adapt to the environment of replit

--- core/db.py
+++ core/db.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient()
+client_url = ""
+client = MongoClient(client_url)
 bot_db = client["discord-bot"]
 extension_server_db = client["discord-bot-extension"]
